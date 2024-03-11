fecd57e27f80be3a980afdcedc4acf4504538e3b
TinntPC <160477784+TinntPC@users.noreply.github.com>
2024-03-11 14:40:53 +0700

    Update LTCB_Bangcuuchuong.cpp

--- LTCB_Bangcuuchuong/LTCB_Bangcuuchuong.cpp
+++ LTCB_Bangcuuchuong/LTCB_Bangcuuchuong.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Bang cuu chuong\n";
+	int so;
+	
 	int cuuchuong[8] = { 2,3,4,5,6,7,8,9 };
 	for (int i = 0; i < 8; i++) {
-		cout << cuuchuong[i] << "\n";
+		for (int j = 1; j <= 10; j++) {
+			cout << cuuchuong[i] << " x " << j << " = " << cuuchuong[i] * j << endl;
+		}
 	}
-	int so;
-	cout << "Nhap so: "; cin >> so;
-	for (int j = 1; j <= 10; j++) {
-		cout << so << " x " << j << " = " << so * j << endl;
-	}
-
 		return 0;
-}
+	}
